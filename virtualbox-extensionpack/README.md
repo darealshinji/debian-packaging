be5f7f937e0657db4671ef81dd72d2dbeb9b47f4
darealshinji <djcj@gmx.de>
2014-12-16 13:36:58 +0100

    vbox-ext: download latest version or optionally a specified version

--- virtualbox-extensionpack/README.md
+++ virtualbox-extensionpack/README.md
@@ -5,3 +5,6 @@ The Oracle VM VirtualBox Extension Pack provides Support for USB 2.0 devices,
 VirtualBox RDP and PXE boot for Intel cards.
 
 https://www.virtualbox.org/
+
+Run `make` to build a package of the latest stable version.
+Enter `./build_specific_version.sh` you want to package a different version.
---
+++ virtualbox-extensionpack/build_specific_version.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+LATEST=$(wget -q -O - http://download.virtualbox.org/virtualbox/LATEST.TXT)
+LATEST_BETA=$(wget -q -O - http://download.virtualbox.org/virtualbox/LATEST-BETA.TXT)
+
+echo "VirtualBox versions available from repositories:"
+echo ""
+echo "Ubuntu 12.04 - 4.1.12"
+echo "Ubuntu 14.04 - 4.3.10"
+echo "Ubuntu 14.10 - 4.3.18"
+echo ""
+echo "Debian wheezy (stable) - 4.1.18"
+echo "Debian wheezy-backports - 4.3.14"
+echo "Debian jessie (testing) - 4.3.18"
+echo ""
+echo "Latest release version - $LATEST"
+echo "Latest beta version - $LATEST_BETA"
+echo ""
+read -p "Enter VirtualBox version (e.g. 4.3.18): " PKG_VERSION
+
+if [ -z $PKG_VERSION ] ; then
+   echo "no version entered"
+   exit 1
+fi
+
+make version=$PKG_VERSION
