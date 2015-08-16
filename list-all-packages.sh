1c1005be9a83964e5012d4333170e76c3bd88a25
darealshinji <djcj@gmx.de>
2015-08-16 12:31:18 +0200

    Don't list mk/patchelfmod as package

--- list-all-packages.sh
+++ list-all-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find */* -maxdepth 0 -type d
-printf "\n`find */* -maxdepth 0 -type d | wc -l` packages\n\n"
+find */* -maxdepth 0 -type d | grep -v -e 'mk/patchelfmod'
+printf "\n`find */* -maxdepth 0 -type d | grep -v -e 'mk/patchelfmod' | wc -l` packages\n\n"
