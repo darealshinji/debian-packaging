3439020f189f199d7a0fcee967aac83224543fa7
darealshinji <djcj@gmx.de>
2015-10-26 03:26:43 +0100

    web/firefox: fix download script

--- web/firefox/download.sh
+++ web/firefox/download.sh
@@ -7,17 +7,18 @@ else
 fi
 
 url="https://download-installer.cdn.mozilla.net/pub/firefox/releases/latest/linux-$arch"
-languages="$(wget -q -O - $url | grep 'folder.gif' | cut -d '>' -f7 | cut -d '/' -f1 | tr '\n' ' ')"
+languages="$(wget -q -O - $url/ | grep "href=\"/pub/firefox/releases/latest/linux-$arch/" | cut -d '>' -f3 | cut -d '/' -f1)"
 
 echo "Firefox is available in many different languages:"
-echo "$languages" | fold -s -w 76 | sed 's/^/  /'
+echo "$languages" | tr '\n' ' ' | fold -s -w 76 | sed 's/^/  /'
+echo
 read -p "Choose your language: [en-US] " fflang
 
 if [ x"$fflang" = x ]; then
     fflang="en-US"
 fi
 
-version="$(wget -q -O - $url/$fflang | grep '.tar.bz2' | cut -d '>' -f7 | cut -d '<' -f1 | sed 's/firefox-//; s/.tar.bz2//;')"
+version="$(wget -q -O - $url/en-US/ | tr '>' '\n' | grep '^firefox-' | sed 's|firefox-||g; s|\.tar\.bz2<\/a||g' | tail -n1)"
 
 rm -f firefox.tbz
 wget -O firefox.tbz "$url/$fflang/firefox-$version.tar.bz2"
