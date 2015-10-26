c622542ed05fd258776cfb8876d525b72112c909
darealshinji <djcj@gmx.de>
2015-10-26 03:26:04 +0100

    web/thunderbird: fix download script

--- web/thunderbird/download.sh
+++ web/thunderbird/download.sh
@@ -7,20 +7,20 @@ else
 fi
 
 url="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/latest/linux-$arch"
-languages="$(wget -q -O - $url | grep 'folder.gif' | cut -d '>' -f7 | cut -d '/' -f1 | tr '\n' ' ')"
+languages="$(wget -q -O - $url/ | grep "href=\"/pub/thunderbird/releases/latest/linux-$arch/" | cut -d '>' -f3 | cut -d '/' -f1)"
 
 echo "thunderbird is available in many different languages:"
-echo "$languages" | fold -s -w 76 | sed 's/^/  /'
+echo "$languages" | tr '\n' ' ' | fold -s -w 76 | sed 's/^/  /'
+echo
 read -p "Choose your language: [en-US] " fflang
 
 if [ x"$fflang" = x ]; then
     fflang="en-US"
 fi
 
-version="$(wget -q -O - $url/$fflang | grep '.tar.bz2' | cut -d '>' -f7 | cut -d '<' -f1 | sed 's/thunderbird-//; s/.tar.bz2//;')"
+version="$(wget -q -O - $url/en-US/ | tr '>' '\n' | grep '^thunderbird-' | sed 's|thunderbird-||g; s|\.tar\.bz2<\/a||g' | tail -n1)"
 
 rm -f thunderbird.tbz
 wget -O thunderbird.tbz "$url/$fflang/thunderbird-$version.tar.bz2"
 echo "1:$version" > VERSION
 sed "s/@L10N@/$fflang/g" debian/control.in > debian/control
-
