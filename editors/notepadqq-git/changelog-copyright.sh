5144b7f8d9f58e8c64fbdd599e5b038bbcba61b7
darealshinji <djcj@gmx.de>
2015-10-30 08:56:41 +0100

    editors/notepadqq-git: generate debian/copyright

--- editors/notepadqq-git/changelog-copyright.sh
+++ editors/notepadqq-git/changelog-copyright.sh
@@ -4,11 +4,12 @@ set -e
 
 . ../../mk/vercmp.inc
 
+cd notepadqq
 
-SRC_VERSION=$(grep -e 'POINTVERSION' notepadqq/src/ui/include/notepadqq.h | \
+SRC_VERSION=$(grep -e 'POINTVERSION' src/ui/include/notepadqq.h | \
               cut -d' ' -f3 | sed -e 's/\"//g')
-TAG_VERSION=$(git -C notepadqq/ describe --abbrev=0 --tags | sed -e 's/v//g')
-latestcommit=$(git -C notepadqq/ log -1 --format=%ci | head -c10 | sed -e 's/-//g')
+TAG_VERSION=$(git describe --abbrev=0 --tags | sed -e 's/v//g')
+latestcommit=$(git log -1 --format=%ci | head -c10 | sed -e 's/-//g')
 
 VERSION=$TAG_VERSION
 git="+git${latestcommit}"
@@ -21,7 +22,7 @@ fi
 
 
 # create new Debian changelog entry
-mv notepadqq/debian/changelog notepadqq/debian/changelog.in
+mv debian/changelog debian/changelog.in
 
 echo "notepadqq (${VERSION}${git}-1~trusty) trusty; urgency=low" > changelog.new
 #echo "notepadqq ($VERSION-1) unstable; urgency=low" > changelog.new
@@ -31,6 +32,20 @@ echo "" >> changelog.new
 #echo " -- `whoami` <`uname -n`>  `date -R`" >> changelog.new
 echo " -- Marshall Banana <djcj@gmx.de>  `date -R`" >> changelog.new
 echo "" >> changelog.new
-cat changelog.new notepadqq/debian/changelog.in > notepadqq/debian/changelog
+cat changelog.new debian/changelog.in > debian/changelog
 rm changelog.new
+
+
+# create debian/copyright
+cat debian/copyright.in > debian/copyright
+for f in `find src/extension_tools -name LICEN?E`; do
+   cat <<EOF >> debian/copyright
+
+
+
+Files: `dirname $f`/*
+
+EOF
+   cat $f >> debian/copyright
+done
 
