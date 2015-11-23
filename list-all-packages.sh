7ac0680b17dea5ae62bb196a038a760e99a5ff11
darealshinji <djcj@gmx.de>
2015-11-23 12:29:14 +0100

    fix listing packages

--- list-all-packages.sh
+++ list-all-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find */* -maxdepth 0 -type d | grep -v -e 'mk/patchelfmod'
-printf "\n`find */* -maxdepth 0 -type d | grep -v -e 'mk/patchelfmod' | wc -l` packages\n\n"
+find */* -maxdepth 0 -type d | grep -v -e '^mk/'
+printf "\n`find */* -maxdepth 0 -type d | grep -v -e '^mk/' | wc -l` packages\n\n"
