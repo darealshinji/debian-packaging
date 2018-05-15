5bded3335e76d612a8507dce63c7b23f765becd4
darealshinji <djcj@gmx.de>
2018-05-15 23:55:18 +0200

    Update pbuilder-satisfydepends.sh

--- mk/pbuilder-satisfydepends.sh
+++ mk/pbuilder-satisfydepends.sh
@@ -2,6 +2,7 @@
 # pbuilder version 0.229
 set -e
 here="$(dirname "$(readlink -f "$0")")"
+export PBUILDER_PKGLIBDIR="$here"
 source "$here/pbuilder-modules"
 source "$here/pbuilder-satisfydepends-funcs"
 source "$here/pbuilder-satisfydepends-aptitude"
